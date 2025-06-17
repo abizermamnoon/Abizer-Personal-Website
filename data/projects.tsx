ea6db6544c1d3b6d37bb22bc75fa63980a1e0a97
Abizer Mamnoon <amamnoon25@cmc.edu>
2025-06-17 14:29:30 -0700

    updated info

--- data/projects.tsx
+++ data/projects.tsx
@@ -1,9 +1,21 @@
 const projects = [
         {
-            title:"Designed a Mini-Wordle",
-            description: "I designed mini-Wordle. This wordle gives you the option to play by yourself, aginst an AI or watch an AI compete with another AI",
+            title:"Determinants of Remote Suitability of Jobs and Residential Distance from the Workplace after the COVID-19 Pandemic",
+            description: (
+            <p>
+            Immediately before the COVID-19 pandemic, workers performed 14% of their paid hours from home, but this figure increased to 38% after the pandemic (Chen et al. 2023).
+            This thesis contributes to our understanding of the determinants of remote work and the factors affecting how far an individual lives from the workplace in the post-pandemic period.
+            I use 1.95 million observations of worker-specific LinkedIn data from August 2023. I construct a measure of remote suitability using Dingel and Neiman’s (2020) method to assign each 
+            job title a remote suitability score based on the associated occupation. I use a Tobit regression to study the effect of an individual's highest level of education, technical skills, 
+            and seniority level on the remote suitability of their job title. I find that post-high school education and seniority above entry level positively impact the remote suitability of an 
+            individual’s job title. An individual with high technical skills related to computing, such as knowledge of programming languages, is also more likely to have a more remote suitable job.
+            I use Tobit model to study the effect of remote suitability, relative housing prices, and relative populations on the distance an individual lives from the workplace. 
+            Surprisingly, individuals with more suitable remote jobs live closer to the workplace. Individuals live further away if the ratio of housing costs closer to the workplace to those in surrounding counties 
+            is higher, but this effect is less pronounced if the remote suitability score of the job is higher. Individuals live closer to the workplace if the ratio of population close to the workplace to those in surrounding counties is higher. 
+            I also find that the interaction between remote suitability and relative populations favors living further away from work if surrounding counties have a lower population.
+            </p>),
             image: "wordle.png",
-            link: "https://github.com/abizermamnoon/Mini-Wordle-with-AI.git",
+            link: "https://scholarship.claremont.edu/cmc_theses/3722/",
             alt: "mini-wordle image"
         }
     ];
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,27 +23,23 @@ export default function Index() {
             </div>
             <div id = "container">
                 <div id = "top-section">
-                <img src = "abizer-pic.png" id = "abizer-pic" />
+                <img src = "abizer_pic.jpg" id = "abizer-pic" />
                 <h1>Hi, nice to meet you! <br/> I'm <ReactTypingEffect text = {["Abizer"]}/> </h1>
 
                 </div>
                 <button className= {(tab ==="about") ? "tab selected": "tab"} onClick = {() => setTab("about")}>About</button>
                 <button className= {(tab ==="blog") ? "tab selected": "tab"} onClick = {() => setTab("blog")}>My Blog</button>
-                <button className= {(tab ==="abizer art") ? "tab selected": "tab"} onClick = {() => setTab("abizer art")}><a href = "https://www.instagram.com/ab_just_for_fun/"> Abizer's Art  </a> </button>
-                <button className= {(tab ==="projects") ? "tab selected": "tab"} onClick = {() => setTab("projects")}>Projects  </button>
-                <button className= {(tab ==="culinary club") ? "tab selected": "tab"} onClick = {() => setTab("culinary club")}><a href = "https://www.instagram.com/claremontculinaryclub/">Culinary Club  </a> </button>
+                <button className= {(tab ==="projects") ? "tab selected": "tab"} onClick = {() => setTab("projects")}>Research  </button>
 
                 {(tab === "about")?(
                     <p>
-                    I am a Sophomore at Claremont McKenna College studying Economics and Data Science. <br/>
-                    <br/>
-                    In the past, I have led a team in which we designed a mini wordle and a stats project that studied the factors affecting Professor ratings and won. <br/><br/>
+                    I am an Economics phD candidate at the University of Notre Dame I graduated from Claremont McKenna <i> Cum Laude </i> with a dual major in Economics and Data Science. <br/><br/>
 
-                    On campus, I am a <a href = "https://www.salvatoricenter.org/">Salvatori Center Research Assistant </a> and I am currently leading a research team in which we are studying the reentry programs for prisoners. I am also a Career Service mentor at <a href = "https://www.cmc.edu/career-services">Soll Center for Student Opportunity</a>. In this role, I help students build their resumes, cover letters, and help the career coaches with administrative tasks.<br/>
-                    I am passionate about the tech industry because it is dynamic. To explore my passion for tech, I am enjoying a web development club where I have been learning web design using CSS, React, MongoDB and engaging with<a href = "https://www.p-ai.org/Project"> AI workshops </a>where I am learning the fundamentals of AI and Machine Learning.
+                   
+                    I am passionate about applied microeconomics. <br/><br/>
+                    In my free time, I like to bake, cycle, play badminton and go on spontaneous hikes.
                     <br/><br/>
-                    I am passionate about the technology industry and I am looking for more experience in product management, SWE, and data science-related fields like Artificial Intelligence or Machine Learning. <br/><br/>
-                    One fun fact about me is that I enjoy cooking and baking. I wanted to share my passion with a like-minded community on campus so I founded a Culinary Club on campus in which we have biweekly meetings where we cook or bake savory/sweet food.
+                    
                     </p>
                 ):(tab === "blog") ? (
                     <p>
@@ -70,8 +66,6 @@ export default function Index() {
                                 title = {d.title}
                                 description = {d.description}
                                 link = {d.link}
-                                image = {d.image}
-                                alt = {d.alt}
                                 />
                             ))}  
                             
